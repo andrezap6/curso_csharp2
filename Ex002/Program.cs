118dce7e15028ba825e2349b5676155ce6b4ed1a
Andreza <andrezap63@gmail.com>
2025-01-15 21:05:35 -0300

    Adicionar arquivos de projeto.

---
+++ Ex002/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            float num = 4.58f;
+            char letra = 'c';
+            string nome = "Andreza";
+
+            Console.WriteLine("O número é {0}.", numero);
+            Console.WriteLine("O valor é R${0}.", num);
+            Console.WriteLine("A letra é {0}.", letra);
+            Console.WriteLine("O nome é {0}.", nome);
+
+            Console.WriteLine("número é {0}, o valor é R${4.58}, a letra é {C} e seu nome é {Andreza}.", numero, num, letra, nome);
+
+
+
+
+
+        }
+    }
+}
