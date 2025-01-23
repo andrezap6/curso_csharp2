6f80b828b498d8ef0b5875142c4bc74d71e95161
Andreza <andrezap63@gmail.com>
2025-01-22 22:09:45 -0300

    excercicio de genero

---
+++ EX015/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EX015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero;
+
+            Console.WriteLine("Digite o gênero: ");
+            genero = Console.ReadLine().ToUpper();
+
+            while (genero != "F" && genero != "M")
+            {
+                Console.Write("Digite o genero: ");
+                genero = Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine("Bem-vindos e bem vindas ao curso de C#!");
+        }
+    }
+}
