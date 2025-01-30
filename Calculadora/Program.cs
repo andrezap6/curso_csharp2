4312c49cadd59b371918422ef020231358a73339
Andreza <andrezap63@gmail.com>
2025-01-29 22:17:36 -0300

    calculo de area do triangulo

---
+++ Calculadora/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+
+            Console.WriteLine("Digite o valor da base: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da altura: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            areatriangulo(num1, num2);
+        }
+
+        static void areatriangulo(double largura, double altura)
+        {
+            double resultado = (largura * altura) / 2;
+            Console.WriteLine($"A área do triangulo é {largura} * {altura} / 2 = {resultado:F2}m²");
+        }
+    }
+}
