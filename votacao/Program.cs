c22936c555f2db962888c06d098136a4db809be1
Andreza <andrezap63@gmail.com>
2025-02-03 20:35:05 -0300

    votacao

---
+++ votacao/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+
+namespace votacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nasc;
+            Console.WriteLine("Digite o ano de nascimento: ");
+            nasc = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(voto(nasc));
+        }
+
+        static string voto(int ano)
+        {
+            // int dia = dataAtual.Day;
+            // int mes = dataAtual.Month;
+            //int hora = dataAtual.Hour;
+            //int minuto = dataAtual.Minute;
+            //int segundo = dataAtual.Second; */
+
+            DateTime dataAtual = DateTime.Now;
+            int anoAtual = dataAtual.Year;
+            int idade = anoAtual - ano;
+
+            if (idade < 16) 
+            {
+                return $"Com {idade} anos, você não vota.";
+            }
+            else if (idade > 18 && idade < 70)
+            {
+                return $"Com {idade} anos, seu voto é obrigatório.";
+            }
+            else
+            {
+                return $"Com {idade} anos, seu voto é opcional.";
+            }   
+
+        }
+
+    }
+}
